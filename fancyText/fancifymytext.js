a198e6c972347f896b85d919e79b8cf4d1fbd4db
Mark Zhang <67395719+AbyssalMager@users.noreply.github.com>
2023-04-22 20:05:12 -0700

    final changes to assignment

--- fancyText/fancifymytext.js
+++ fancyText/fancifymytext.js
@@ -1,1 +1,22 @@
-alert("Hello, world!");
+function textBigger(){
+	document.getElementById("inputText").style.fontSize = "2em";
+}
+
+function boldText(){
+	document.getElementById("inputText").style.fontWeight = "bold";
+	document.getElementById("inputText").style.color = "blue";
+	document.getElementById("inputText").style.textDecoration = "underline";
+}
+
+function revertText(){
+	document.getElementById("inputText").style.fontWeight = "normal";
+	document.getElementById("inputText").style.color = "black";
+	document.getElementById("inputText").style.textDecoration = "initial";
+}
+
+function moo(){
+	var sentence = document.getElementById("inputText").value;
+	var moo = sentence.split(".");
+	sentence = moo.join("-Moo.");
+	document.getElementById("inputText").value = sentence;
+}
